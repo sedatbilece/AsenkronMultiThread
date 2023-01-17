26438f141b4f8279fcac338d070c08a4826c05ab
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-17 11:14:56 +0300

    async structure in api

---
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWebApp.API.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> getContentAsync()
+        {
+            var mytext =  new HttpClient()
+                .GetStringAsync("https://www.google.com");
+
+            //
+
+            var data = await  mytext;
+
+            return Ok(data);
+        }
+        
+    }
+}

9be9eb74e28ca40a79314e5b8bfed6538390c8f6
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-19 09:19:20 +0300

    ContinueWith method

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+var mytask =  new HttpClient().GetStringAsync("https://www.google.com")
+    .ContinueWith((data) =>
+    {
+        Console.WriteLine(data.Result.Length);
+    });
+
+
+
+Console.WriteLine("ara işlemler");
+
+await mytask;
